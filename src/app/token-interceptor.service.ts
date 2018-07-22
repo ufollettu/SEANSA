4fd51ff52cf49db2445fdb76d8aa83baf4b40543
Pasquale Merolle <pasquale.merolle@gmail.com>
2018-07-22 19:24:12 +0200

    fix token interceptor

--- src/app/token-interceptor.service.ts
+++ src/app/token-interceptor.service.ts
@@ -1,6 +1,7 @@
+import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
 import { Injectable, Injector } from '@angular/core';
-import { HttpInterceptor } from '@angular/common/http';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -9,13 +10,27 @@ export class TokenInterceptorService implements HttpInterceptor {
 
   constructor(private injector: Injector) { }
 
-  intercept(req, next) {
-    const authService = this.injector.get(AuthService);
-    const tokenizedReq = req.clone({
-      setHeaders: {
-        Authorization: `Bearer ${authService.getToken()}`
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+      const authService = this.injector.get(AuthService);
+      const token: string = authService.getToken();
+
+      if (token) {
+        console.log(token);
+          req = req.clone({
+            setHeaders: {
+              Authorization: `Bearer ${token}`
+            }
+          });
       }
-    });
-    return next.handle(tokenizedReq);
-  }
+      // if (!req.headers.has('Content-Type')) {
+      //     req = req.clone({
+      //       headers: req.headers.set('Content-Type', 'application/json')
+      //     });
+      // }
+      // req = req.clone({
+      //   headers: req.headers.set('Accept', 'application/json')
+      // });
+
+      return next.handle(req);
+    }
 }
